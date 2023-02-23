eca95a44b57fdb61194d58059bc0eb1a6442c407
mithilesh2023 <mithilesht2023@gmail.com>
2023-02-23 10:10:31 +0530

    first commit

---
+++ shop/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+
+class Category(models.Model):
+    title=models.CharField(max_length=200)
+    description=models.TextField(null=True)
+    slug=models.SlugField()
+
+    def __str__(self):
+        return self.title
