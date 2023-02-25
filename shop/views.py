5d469c46169740c9ff4c7925aa41216c14f6a830
mithilesh2023 <mithilesht2023@gmail.com>
2023-02-25 19:23:00 +0530

    cards done

--- shop/views.py
+++ shop/views.py
@@ -1,9 +1,24 @@
 from django.shortcuts import render
 from shop.models import *
+# from .models import Product
+
+
 # Create your views here.
-def getCategory():
-    return Category.objects.all()
+
+# def getCategory():
+#     return Category.objects.all()
+    # data={
+    #     'categoryData':catrgoryData
+    # }
+    # return render(r,'home.html',data)
 def homepage(r):
-    data={}
-    data['category']=getCategory()
+    categoryData=Category.objects.all()
+    productData=Product.objects.all()[:8]
+    data={
+        'categoryData':categoryData,
+        'productData':productData
+    }
+    # categoryData:{}
+    # categoryData['category']=getCategory()
+    # data['category']=getCategory()
     return render(r, "home.html",data)
